bff8f858b74c1d8829e05e97b7e3c2dd937cf8a5
chintansavla99 <chintansavla99@gmail.com>
2020-07-25 02:38:36 +0530

    plot3.R and plot3.png

---
+++ plot3.R
@@ -0,0 +1,13 @@
+dt<-read.table("household_power_consumption.txt", sep = ";",header = TRUE)
+dt$Date<-as.Date(dt$Date,"%d/%m/%Y")
+library(dplyr)
+dat<-filter(dt,Date==as.Date("2007-02-01")|Date==as.Date("2007-02-02"))
+rm(dt)
+dat$datetime<-strptime(paste(dat$Date,dat$Time),format = "%Y-%m-%d %H:%M:%S")
+
+plot(dat$datetime,as.numeric(dat$Sub_metering_1),ylab = "Energy sub metering",xlab = "",xlim = c(as.POSIXct("2007-02-01 00:00:00",format = "%Y-%m-%d %H:%M:%S"),as.POSIXct("2007-02-03 00:00:00",format = "%Y-%m-%d %H:%M:%S")),type = 'l')
+lines(dat$datetime,dat$Sub_metering_2,col="red")
+lines(dat$datetime,dat$Sub_metering_3,col="blue")
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,cex = 0.7)
+dev.copy(png,"plot3.png")
+dev.off()
